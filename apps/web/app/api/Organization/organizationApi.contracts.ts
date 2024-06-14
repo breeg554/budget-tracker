5db4895f09ab43e7508704842f965e10f3bbc6d5
breeg554 <dvdk98@gmail.com>
2024-06-14 22:52:37 +0200

    update routes

---
+++ apps/web/app/api/Organization/organizationApi.contracts.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+import { getTransactionSchema } from "~/api/Transaction/transactionApi.contracts";
+
+export const getOrganizationSchema = z.object({
+  id: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  name: z.string(),
+  users: z.array(z.string()),
+  transactions: z.array(getTransactionSchema),
+});
+
+export const fromGetOrganizationResponse = z.array(getOrganizationSchema);
