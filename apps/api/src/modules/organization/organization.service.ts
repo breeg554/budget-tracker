f95cb4b6b251b64fa0bfb70e0369e81dd1b7319a
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-06-22 17:32:34 +0200

    refactor

--- apps/api/src/modules/organization/organization.service.ts
+++ apps/api/src/modules/organization/organization.service.ts
@@ -5,7 +5,6 @@ import { Organization } from '~/entities/organization/organization.entity';
 import { CreateOrganizationDto } from '~/dtos/organization/create-organization.dto';
 import { User } from '~/entities/user/user.entity';
 import { UserService } from '~/modules/organization/user/user.service';
-import { OrganizationNotFoundException } from '~/modules/errors/organization-not-found.exception';
 
 @Injectable()
 export class OrganizationService {
@@ -48,25 +47,22 @@ export class OrganizationService {
   }
 
   async findByNameAndUser(name: string, userId: string): Promise<Organization> {
-    const organization = await this.organizationRepository.findOne({
-      where: {
-        name: name,
-      },
-      relations: ['users'],
-    });
-
-    if (!organization) {
-      throw new OrganizationNotFoundException();
-    }
-
-    await this.ensureUserInOrganization(userId, organization);
-
-    return organization;
+    return await this.ensureUserInOrganization(userId, name);
   }
 
-  async ensureUserInOrganization(userId: string, organization: Organization) {
-    if (!organization.users.some((orgUser) => orgUser.id === userId)) {
+  async ensureUserInOrganization(
+    userId: string,
+    organizationName: string,
+  ): Promise<Organization> {
+    const organization = await this.findByName(organizationName);
+
+    if (
+      !organization ||
+      !organization.users.some((orgUser) => orgUser.id === userId)
+    ) {
       throw new NotFoundException('Organization not found');
     }
+
+    return organization;
   }
 }
