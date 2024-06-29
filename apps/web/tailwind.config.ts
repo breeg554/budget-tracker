52f80c666cce03e3459e31aba209feb878734a0e
kielbasa-elp <dawid.kielbasa@elpassion.pl>
2024-06-29 19:36:41 +0200

    setup shadcn

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,58 +1,80 @@
-import type { Config } from "tailwindcss";
-import defaultTheme from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss"
 
-export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--default-font-family)", ...defaultTheme.fontFamily.sans],
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
+    },
+    extend: {
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          50: "var(--purple-1)",
-          100: "var(--purple-2)",
-          150: "var(--purple-3)",
-          200: "var(--purple-4)",
-          250: "var(--purple-5)",
-          300: "var(--purple-6)",
-          400: "var(--purple-7)",
-          500: "var(--purple-8)",
-          600: "var(--purple-9)",
-          700: "var(--purple-10)",
-          800: "var(--purple-11)",
-          900: "var(--purple-12)",
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-          50: "var(--orange-1)",
-          100: "var(--orange-2)",
-          150: "var(--orange-3)",
-          200: "var(--orange-4)",
-          250: "var(--orange-5)",
-          300: "var(--orange-6)",
-          400: "var(--orange-7)",
-          500: "var(--orange-8)",
-          600: "var(--orange-9)",
-          700: "var(--orange-10)",
-          800: "var(--orange-11)",
-          900: "var(--orange-12)"
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
-        neutral: {
-          50: "var(--gray-1)",
-          100: "var(--gray-2)",
-          150: "var(--gray-3)",
-          200: "var(--gray-4)",
-          250: "var(--gray-5)",
-          300: "var(--gray-6)",
-          400: "var(--gray-7)",
-          500: "var(--gray-8)",
-          600: "var(--gray-9)",
-          700: "var(--gray-10)",
-          800: "var(--gray-11)",
-          900: "var(--gray-12)",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
