0457db95b9c44ac56abc890a80052f2a72ec55f4
breeg554 <dvdk98@gmail.com>
2024-08-17 23:28:33 +0200

    prompt engineering

--- apps/api/src/modules/organization/receipt/receipt.service.ts
+++ apps/api/src/modules/organization/receipt/receipt.service.ts
@@ -44,16 +44,19 @@ export class ReceiptService {
           role: 'system',
           content: `You are a receipt processing bot. 
             I will send you shop receipt in base64 format and your job is to get products from it.
+            
+            The most common receipt language is Polish, but you can also get receipts in other languages.
+            
             Return them in json format of that shape
             ---
             {
-              content: TEXT_FROM_RECEIPT,
+              content: ALL TEXT FROM RECEIPT THAT YOU USED TO GET PRODUCTS,
               products: [
                 {
                   name: PRODUCT_NAME,
                   price: PRICE_OF_SINGLE_ITEM,
                   quantity: QUANTITY_OF_ITEMS (if you notice the same product multiple times in the receipt, sum the quantity up)
-                  category: ID_OF_CATEGORY (if you can't match the product to any category, use the ID of the "Other" category)
+                  category: ID_OF_CATEGORY (if you can't match the product to any category, use the ID of the "other" category). Always use the most matching category for the product! The "other" category is the last resort.
                   categoryName: NAME_OF_CATEGORY,
                 }
               ]
@@ -64,10 +67,13 @@ export class ReceiptService {
             ---
               ${preparedCategories}
             ---
-            
-            DO not return anything else! 
-            
-            If you cannot retrieve any products or content from image, return empty array for "products" and empty string for "content".
+         
+            Remember that:
+            1. When you see product weight multiplied by price (e.g. 0.5kg x 3.00LN), you should look for the final price (that should be listed somewhere on the right side). In this case, the final price is 1.50PLN and quantity is 1!        
+            2. Always get as many products as you can and try to not miss any.
+            3. At the beginning of the receipt, there is always a shop name and address. You can ignore it. The same goes for the footer of the receipt. Products should be in the middle of the receipt.
+            4. If you cannot retrieve any products or content from image, return empty array for "products" and empty string for "content".
+            5. DO NOT return anything else than products and content. 
             `,
         },
         {
