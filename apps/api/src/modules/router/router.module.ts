1cd9ae39f8414190cb80a594c14cc8d9eea08e2f
breeg554 <dvdk98@gmail.com>
2024-08-29 20:12:41 +0200

    update RouterModule

--- apps/api/src/modules/router/router.module.ts
+++ apps/api/src/modules/router/router.module.ts
@@ -3,24 +3,30 @@ import { OrganizationModule } from '~/modules/organization/organization.module';
 import { TransactionModule } from '~/modules/organization/transaction/transaction.module';
 import { StatisticsModule } from '~/modules/organization/statistics/statistics.module';
 import { ReceiptModule } from '~/modules/organization/receipt/receipt.module';
+import { Module } from '@nestjs/common';
 
-export const RouterModule = NestRouterModule.register([
-  {
-    path: 'organizations',
-    module: OrganizationModule,
-    children: [
+@Module({
+  imports: [
+    NestRouterModule.register([
       {
-        path: '/:organizationName/transactions',
-        module: TransactionModule,
+        path: 'organizations',
+        module: OrganizationModule,
+        children: [
+          {
+            path: '/:organizationName/transactions',
+            module: TransactionModule,
+          },
+          {
+            path: '/:organizationName/statistics',
+            module: StatisticsModule,
+          },
+          {
+            path: '/:organizationName/receipts',
+            module: ReceiptModule,
+          },
+        ],
       },
-      {
-        path: '/:organizationName/statistics',
-        module: StatisticsModule,
-      },
-      {
-        path: '/:organizationName/receipts',
-        module: ReceiptModule,
-      },
-    ],
-  },
-]);
+    ]),
+  ],
+})
+export class RouterModule {}
