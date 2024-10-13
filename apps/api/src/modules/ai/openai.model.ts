094ec1f04e5c58bd491b48f23613667a32607665
breeg554 <dvdk98@gmail.com>
2024-10-13 14:20:52 +0200

    update openai model

--- apps/api/src/modules/ai/openai.model.ts
+++ apps/api/src/modules/ai/openai.model.ts
@@ -17,7 +17,7 @@ export class OpenAIModel extends AiModel {
     super();
 
     this.client = new ChatOpenAI({
-      model: 'gpt-4o-mini-2024-07-18',
+      model: 'gpt-4o-2024-08-06',
       ...args,
     });
 
